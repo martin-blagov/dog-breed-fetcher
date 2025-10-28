0ab4242b0985a55b9758c3b08b4f4a5e30ab03bd
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2025-10-27 20:14:19 -0400

    implement breedfetcher interface and update code in breedfetcherforlocaltesting

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -13,7 +13,6 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
     private int callsMade = 0;
     private BreedFetcher fetcher;
     private Map<String, List<String>> cache = new HashMap<>();
